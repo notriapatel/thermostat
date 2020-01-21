6bf6c88bb6efa166b1ddbdc5d30e1f409704251c
ria patel <ria.amy.patel@gmail.com>
2020-01-21 17:12:07 +0000

    added min temp

--- jasmine-standalone-3.5.0/spec/ThermostatSpec.js
+++ jasmine-standalone-3.5.0/spec/ThermostatSpec.js
@@ -9,17 +9,26 @@ describe('Thermostat', function() {
 
   describe('when initializing', function() {
     it('the starting temperature is 20 degrees', function() {
-      expect(thermostat.startingTemperature()).toEqual(20);
+      expect(thermostat.currentTemperature()).toEqual(20);
     });
   });
 
   describe('changing temperature', function() {
     it('by increasing it', function() {
-      expect(thermostat.increaseTemperature(10)).toEqual(30);
+      thermostat.up();
+      expect(thermostat.currentTemperature()).toEqual(21);
     });
 
     it('by decreasing it', function() {
-      expect(thermostat.decreaseTemperature(10)).toEqual(10);
+      thermostat.down();
+      expect(thermostat.currentTemperature()).toEqual(19);
+    });
+
+    it('has a minimum of 10 degrees', function() {
+      for (var i = 0; i < 11; i++) {
+        thermostat.down();
+      }
+      expect(thermostat.currentTemperature()).toEqual(10);
     });
   });
 });
--- jasmine-standalone-3.5.0/src/Thermostat.js
+++ jasmine-standalone-3.5.0/src/Thermostat.js
@@ -1,15 +1,25 @@
+'use strict';
+
 function Thermostat() {
   this.temperature = 20;
+  this.MINIMUM_TEMPERATURE = 10;
 }
 
-Thermostat.prototype.startingTemperature = function() {
+Thermostat.prototype.currentTemperature = function() {
   return this.temperature;
 };
 
-Thermostat.prototype.increaseTemperature = function(number) {
- return this.temperature + number;
+Thermostat.prototype.up = function() {
+ return this.temperature += 1;
 };
 
-Thermostat.prototype.decreaseTemperature = function(number) {
-  return this.temperature - number;
+Thermostat.prototype.down = function() {
+  if (this.isMinimumTemperature()) {
+    return;
+  }
+    this.temperature -= 1; 
+};
+
+Thermostat.prototype.isMinimumTemperature = function() {
+  return this.temperature === this.MINIMUM_TEMPERATURE;
 };
