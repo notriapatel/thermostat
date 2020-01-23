8eceabc33b65a3f0ba210a2e33825685c9a8720e
ria patel <ria.amy.patel@gmail.com>
2020-01-23 16:01:43 +0000

    adding weather

--- jasmine-standalone-3.5.0/src/interface.js
+++ jasmine-standalone-3.5.0/src/interface.js
@@ -2,9 +2,30 @@ $(document).ready(function() {
   var thermostat = new Thermostat();
   updateTemp();
 
+  $('#current-city').change(function() {
+    var city = $('#current-city').val();
+    $.get('https://api.openweathermap.org/data/2.5/weather?q=' + city +'&appid=9c4f3f161177d59178b177a4f58243bc', function(data) {
+      $('#current-temp').text(data.main.temp);
+    })
+
+    $.get('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=9c4f3f161177d59178b177a4f58243bc', function(data) {
+      $('#city').text(data.name);
+    })
+  })
+
+  // $.get('https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=9c4f3f161177d59178b177a4f58243bc', function(data) {
+  //   $('#current-temp').text(data.main.temp);
+  // })
+
+  // $.get('https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=9c4f3f161177d59178b177a4f58243bc', function(data) {
+  //   $('#city').text(data.name);
+  // })
+
+
   function updateTemp() {
     $('#temperature').text(thermostat.temperature);
     $('#temperature').attr('class', thermostat.usage());
+  
   }
 
   $('#temperature-up').on('click', function() {
