a29045585ce87ae2b62931e014206e9f53e5337f
shaunlsy <shaunlowshuennyuh@hotmail.com>
2020-01-23 16:54:44 +0000

    add cartoon thermostat

--- jasmine-standalone-3.5.0/src/interface.js
+++ jasmine-standalone-3.5.0/src/interface.js
@@ -1,57 +1,49 @@
 $(document).ready(function() {
-  var thermostat = new Thermostat();
-  updateTemp();
+    var thermostat = new Thermostat();
+    updateTemp();
 
-  $('#current-city').change(function() {
-    var city = $('#current-city').val();
-    $.get('https://api.openweathermap.org/data/2.5/weather?q=' + city +'&appid=9c4f3f161177d59178b177a4f58243bc', function(data) {
-      $('#current-temp').text(data.main.temp);
-    })
+    $("#current-city").change(function() {
+        var city = $("#current-city").val();
+        displayWeather(city);
+        $("#city").text(city);
+    });
 
-    $.get('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=9c4f3f161177d59178b177a4f58243bc', function(data) {
-      $('#city').text(data.name);
-    })
-  })
+    function displayWeather(city) {
+        var url = "https://api.openweathermap.org/data/2.5/weather?q=" + city;
+        var token = "&appid=9c4f3f161177d59178b177a4f58243bc";
+        var units = "&units=metric";
+        $.get(url + token + units, function(data) {
+            $("#current-temp").text(data.main.temp);
+        });
+    }
 
-  // $.get('https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=9c4f3f161177d59178b177a4f58243bc', function(data) {
-  //   $('#current-temp').text(data.main.temp);
-  // })
+    function updateTemp() {
+        $("#temperature").text(thermostat.temperature);
+        $("#temperature").attr("class", thermostat.usage());
+    }
 
-  // $.get('https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=9c4f3f161177d59178b177a4f58243bc', function(data) {
-  //   $('#city').text(data.name);
-  // })
+    $("#temperature-up").on("click", function() {
+        thermostat.up();
+        updateTemp();
+    });
 
+    $("#temperature-down").on("click", function() {
+        thermostat.down();
+        updateTemp();
+    });
 
-  function updateTemp() {
-    $('#temperature').text(thermostat.temperature);
-    $('#temperature').attr('class', thermostat.usage());
-  
-  }
+    $("#temperature-reset").on("click", function() {
+        thermostat.resetTemp();
+        updateTemp();
+    });
 
-  $('#temperature-up').on('click', function() {
-    thermostat.up();
-    updateTemp()  
-  })
+    $("#powersaving-on").on("click", function() {
+        thermostat.turnOnPowerSavingMode();
+        updateTemp();
+    });
 
-  $('#temperature-down').on('click', function() {
-    thermostat.down();
-    updateTemp()
-  })
-
-  $('#temperature-reset').on('click', function() {
-    thermostat.resetTemp();
-    updateTemp()
-  })
-
-  $('#powersaving-on').on('click', function() {
-    thermostat.turnOnPowerSavingMode();
-    updateTemp()
-  })
-
-  $('#powersaving-off').on('click', function() {
-    thermostat.turnOffPowerSavingMode();
-    updateTemp()
-  })
-  
-})
-
+    $("#powersaving-off").on("click", function() {
+        thermostat.turnOffPowerSavingMode();
+        updateTemp();
+    });
+});
